62216c7fe2d1f70a583a9522b37d7f3b63be04cb
Nigel Doheny <nigel.doheny93@gmail.com>
2022-09-11 17:06:49 +0000

    Amend Allowed_hosts

--- bottega_pia/settings.py
+++ bottega_pia/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["bottega_pia.herokupp.com", "localhost"]
+ALLOWED_HOSTS = ["bottegapia.herokupp.com", "localhost"]
 
 
 # Application definition
