b848baa74103c3601bf040799901d6958a9263aa
Nigel Doheny <nigel.doheny93@gmail.com>
2022-09-27 12:58:38 +0000

    Amend reservations to auto populate when user is logged in

--- reservations/views.py
+++ reservations/views.py
@@ -9,6 +9,7 @@ import datetime
 from bootstrap_datepicker_plus.widgets import DateTimePickerInput, DatePickerInput, TimePickerInput
 from django.contrib.auth.models import User
 import logging
+from django.contrib.auth.decorators import login_required
 
 logger = logging.getLogger(__name__)
 
@@ -18,12 +19,20 @@ logger = logging.getLogger(__name__)
 class ReservationsEnquiry(View):
     template_name = "reservations.html"
     def get(self, request, *args, **kwargs):    
-        customer_form = CustomerForm()
-        reservation_form = ReservationForm()
+        if request.user.is_authenticated:
+            customer_form = CustomerForm(initial={'full_name': request.user.first_name + " " + request.user.last_name, 'email': request.user.email})
+            reservation_form = ReservationForm()
+            return render(
+                request, self.template_name, 
+                {'customer_form': customer_form, 'reservation_form': reservation_form}
+                )
+        else:
+            messages.add_message(
+                        request, messages.ERROR, "You must be logged in to make a reservation.")
         logger.warning("Get request")
+        
         return render(
-            request, self.template_name, 
-            {'customer_form': customer_form, 'reservation_form': reservation_form}     
+            request, self.template_name,  
         )
 
     def post(self, request, *args, **kwargs):
@@ -32,38 +41,46 @@ class ReservationsEnquiry(View):
         max_tables = Table.objects.all().count()
 
         logger.warning(f"Maximum number of tables: {max_tables}")
-        if customer_form.is_valid() and reservation_form.is_valid():
-            customer_requested_time = reservation_form.cleaned_data['requested_time']
-            customer_requested_date = reservation_form.cleaned_data['requested_date']
+        # You must be logged in to make in a reservation enquiry
+        @login_required
+        def send_reservation(self, customer_form, reservation_form, max_tables, request):
+            if customer_form.is_valid() and reservation_form.is_valid():
+                
+                customer_requested_time = reservation_form.cleaned_data['requested_time']
+                customer_requested_date = reservation_form.cleaned_data['requested_date']
+                logger.warning(f"{customer_requested_time}, {customer_requested_date}")
 
-            # def get_no_tables_booked(customer_requested_date, customer_requested_time):
-
-            logger.warning(f"{customer_requested_time}, {customer_requested_date}")
-            queryset = len(Reservation.objects.filter(
-                requested_time=customer_requested_time, requested_date=customer_requested_date, status="confirmed")
+                queryset = len(Reservation.objects.filter(
+                    requested_time=customer_requested_time, requested_date=customer_requested_date, status="confirmed")
                 )
 
-            logger.warning(f"{queryset}")
-            print(queryset)
-            if queryset == max_tables:
+                logger.warning(f"{queryset}")
+                print(queryset)
+                if queryset == max_tables:
 
-                messages.add_message(
-                    request, messages.ERROR, f"Unfortunately we are fully booked at {customer_requested_time} on {customer_requested_date}")
+                    messages.add_message(
+                        request, messages.ERROR, f"Unfortunately we are fully booked at {customer_requested_time} on {customer_requested_date}")
+
+                    return render(
+                    request, 'reservations.html', 
+                    {'customer_form': customer_form, 'reservation_form': reservation_form}
+                    )   
+                else:
+                    customer_form.save()
+                    reservation_form.save()    
+            
+                    messages.add_message(
+                                request, messages.SUCCESS, f"Your enquiry for {customer_requested_time} on {customer_requested_date} has been sent - please note this is not approved until you receive a confirmaton email.")
+
+                    return render(request, 'reservations.html')
+            else:
+                messages.add_message(request, messages.ERROR, "Something is not right with your form")
 
                 return render(
-                request, 'reservations.html', 
-                {'customer_form': customer_form, 'reservation_form': reservation_form}
-                )  
-            else:
-                customer_form.save()
-                reservation_form.save()
-                messages.add_message(
-                        request, messages.SUCCESS, "Your enquiry has been sent - please note this is not approved until you receive a confirmaton email.")
-
-                return render(request, 'reservations.html')
-        else:
-            messages.add_message(request, messages.ERROR, "Something is not right with your form")
+                    request, 'reservations.html', 
+                    {'customer_form': customer_form, 'reservation_form': reservation_form}
+                )
             return render(
-                request, 'reservations.html', 
-                {'customer_form': customer_form, 'reservation_form': reservation_form}
-                ) 
+                    request, 'reservations.html',
+                    {'customer_form': customer_form, 'reservation_form': reservation_form}
+                )      
