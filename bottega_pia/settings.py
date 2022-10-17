f2ad25c5f8e37ea1fdd65bec3edb58293a140885
Nigel Doheny <nigel.doheny93@gmail.com>
2022-10-17 11:25:24 +0000

    Add email settings to enable gmail smtp to send contact form from User

--- bottega_pia/settings.py
+++ bottega_pia/settings.py
@@ -64,10 +64,18 @@ INSTALLED_APPS = [
 SITE_ID = 1
 
 # Email settings
-EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+# EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_USE_TLS = True
+EMAIL_PORT = 587
+EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER')
+EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASSWORD')
+
+# Account settings
 ACCOUNT_AUTHENTICATION_METHOD = 'username_email'
 ACCOUNT_EMAIL_REQUIRED = True
-# ACCOUNT_EMAIL_VERIFICATION = 'mandatory'
+ACCOUNT_EMAIL_VERIFICATION = 'mandatory'
 ACCOUNT_SIGNUP_EMAIL_ENTER_TWICE = True
 ACCOUNT_USERNAME_MIN_LENGTH = 4
 LOGIN_URL = '/accounts/login/'
--- website/views.py
+++ website/views.py
@@ -4,6 +4,8 @@ from .forms import ContactForm
 from django.http import HttpResponseRedirect
 from django.contrib import messages
 from django.contrib.auth.models import User
+from django.core.mail import send_mail
+from django.conf import settings
 
 # Create your views here.
 def index(request):
@@ -20,7 +22,15 @@ def drinks_menu(request):
     return render(request, 'drinks_menu.html')  
 
 def reservations(request):
-    return render(request, 'reservations.html')     
+    return render(request, 'reservations.html') 
+
+def send_message(request, contact_form):
+    customer_name = contact_form.cleaned_data['name']
+    subject = (f'Message from {customer_name}')
+    message = contact_form.cleaned_data['message']
+    email_from = contact_form.cleaned_data['email']
+    recipient_list = [settings.EMAIL_HOST_USER]
+    send_mail( subject, message, email_from, recipient_list )        
 
 class ContactPage(View):
     """
@@ -45,6 +55,7 @@ class ContactPage(View):
 
             if form.is_valid():
                 # Return blank form so the same message isn't posted twice.
+                send_message(request, contact_form)
                 contact_form = ContactForm()
                 messages.add_message(
                     request, messages.SUCCESS, "Thank you for contacting us, one of our staff will be in touch shortly. <br>For anything urgent please call on +39 329 277 0444"
