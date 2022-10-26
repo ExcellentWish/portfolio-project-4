8bca4eb35a603fed3e19cfddaababaf06860e8ca
Nigel Doheny <nigel.doheny93@gmail.com>
2022-10-26 09:36:18 +0000

    Change submit button style

--- reservations/models.py
+++ reservations/models.py
@@ -60,7 +60,7 @@ class Reservation(models.Model):
     no_of_guests = models.IntegerField(choices=guests_choices, default=1)
     requested_time = models.DateTimeField()
     requested_date = models.DateField(default=datetime.date.today)
-    requested_time = models.CharField(max_length=10, choices=time_choices, default='12:00')
+    requested_time = models.CharField(max_length=10, choices=time_choices, default='12:30')
     table_id = models.ForeignKey(Table, on_delete=models.CASCADE, related_name="table_booked", null=True)
     status_choices = ((0, "pending"), (1, "confirmed"))
     status = models.CharField(
