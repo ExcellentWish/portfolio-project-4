b94153f4680a9d0b105efeeae1fc205388a3eab7
Nigel Doheny <nigel.doheny93@gmail.com>
2022-10-31 11:07:10 +0000

    Add functions to script.js to hide map when using smaller screens

--- static/js/script.js
+++ static/js/script.js
@@ -21,4 +21,37 @@ $(document).ready(function() {
     }
 
     window.initMap = initMap;
+    
+    function screenSize() {
+        if (window.innerWidth < 994) {
+            $('#map').addClass('hidden');
+        } else {
+            $('#map').removeClass('hidden');
+        }
+    }
+
+    function debounce(func, wait, immediate) {
+        var timeout;
+        return function () {
+            var context = this,
+                args = arguments;
+            var later = function () {
+                timeout = null;
+                if (!immediate) func.apply(context, args);
+            };
+            var callNow = immediate && !timeout;
+            clearTimeout(timeout);
+            timeout = setTimeout(later, wait);
+            if (callNow) func.apply(context, args);
+        };
+    }
+    
+    var screenChangeEfficient = debounce(function () {
+        screenSize();
+    }, 150);
+
+
+    window.addEventListener('resize', screenChangeEfficient);
+    
+    screenSize();
 });
