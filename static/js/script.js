0f9e1a0a3654226ee7e53ac07b5de93cb7876d65
Nigel Doheny <nigel.doheny93@gmail.com>
2022-11-01 10:45:13 +0000

    Add map.js to have maps in it seprate file so code is tidier

--- static/js/script.js
+++ static/js/script.js
@@ -2,25 +2,6 @@ $(document).ready(function() {
     console.log('Working')
     $("#id_requested_date").datepicker({ dateFormat: 'dd/mm/yy' });
 
-    function initMap() {
-        // The location of Bottega Pia 41.7312638,12.6584794
-        const bottega = {
-            lat: 41.7312638,
-            lng: 12.6584794
-        };
-        // The map, centered at Bottega Pia
-        const map = new google.maps.Map(document.getElementById("map"), {
-            zoom: 20,
-            center: bottega,
-        });
-        // The marker, positioned at Bottega Pia
-        const marker = new google.maps.Marker({
-            position: bottega,
-            map: map,
-        });
-    }
-
-    window.initMap = initMap;
     
     function screenSize() {
         if (window.innerWidth < 994) {
