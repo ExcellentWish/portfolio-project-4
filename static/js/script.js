ba28bbf9f5427586bfc1314884d506677b7aa7cf
Nigel Doheny <nigel.doheny93@gmail.com>
2022-11-14 12:04:48 +0000

    Delete maps.js and add to script.js. So all JS is together

--- static/js/script.js
+++ static/js/script.js
@@ -91,4 +91,26 @@ $(document).ready(function () {
     window.addEventListener('resize', screenChangeEfficient);
 
     screenSize();
+
+
+    function initMap() {
+        // The location of Bottega Pia 41.7312638,12.6584794
+        const bottega = {
+            lat: 41.7312638,
+            lng: 12.6584794
+        };
+        // The map, centered at Bottega Pia
+        const map = new google.maps.Map(document.getElementById("map"), {
+            zoom: 20,
+            center: bottega,
+        });
+        // The marker, positioned at Bottega Pia
+        const marker = new google.maps.Marker({
+            position: bottega,
+            map: map,
+        });
+    }
+    
+    window.initMap = initMap;
+    
 });
